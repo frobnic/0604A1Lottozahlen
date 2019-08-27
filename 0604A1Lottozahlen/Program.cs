1a0de49e671b0a47dcb0d3c0af8a42c6437b5f76
frank paulsen <frobnic+ita@gmail.com>
2019-08-27 08:45:09 +0200

    inserted code

--- 0604A1Lottozahlen/Program.cs
+++ 0604A1Lottozahlen/Program.cs
@@ -10,6 +10,33 @@ namespace _0604A1Lottozahlen
     {
         static void Main(string[] args)
         {
+            int[] zahlen = new int[6];
+            int anzahl = 0, zuf = 0;
+            Random zufall = new Random();
+            Boolean found = false;
+
+            do
+            {
+                zuf = zufall.Next(1, 50);
+                found = false;
+                for (int i = 0; i < 6; i++)
+                    if (zahlen[i] == zuf)
+                    {
+                        found = true;
+                        break;
+                    }
+                if (!found)
+                {
+                    zahlen[anzahl] = zuf;
+                    anzahl++;
+                }
+            } while (anzahl < 6);
+
+            foreach (int a in zahlen)
+                Console.Write("{0} ", a);
+
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
